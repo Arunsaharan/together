fc988278ea81dccf6c220787bafcd70585d658ec
vikaspanwar06 <you@example.com>
2025-01-19 18:24:05 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css';
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import HomePage from './pages/homepage/Homepage';
+import Personalize from './pages/personalize/Personalize';
+import CreateTrip from './pages/create-trip/CreateTrip';
+import ExploreHolidays from './pages/explore-holidays/ExploreHolidays';
+import Agencies from './pages/agencies/Agencies';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+
+function App() {
+
+  return (
+    <div>
+      <Navbar />
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/personalize" element={<Personalize />} />
+            <Route path="/create-trip" element={<CreateTrip />} />
+            <Route path="/explore-holidays" element={<ExploreHolidays />} />
+            <Route path="/agencies" element={<Agencies />} />
+          </Routes>
+        </div>
+      </Router>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/explore-holidays/ExploreHolidays.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExploreHolidays = () => {
+  return (
+    <div>ExploreHolidays</div>
+  )
+}
+
+export default ExploreHolidays
